4deadd35d270a67dd7ba651d9552f06c436a7f3a
Simone Moretti <info@morettisimone.com>
2026-06-23 00:31:11 +0200

    Add user service: registration, authentication and token generation

---
+++ app/services/user_service.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.utils.security import hash_password, verify_password, create_access_token
+
+def get_user_by_email(db: Session, email: str) -> User | None:
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_user_by_username(db: Session, username: str) -> User | None:
+    return db.query(User).filter(User.username == username).first()
+
+def create_user(db: Session, username: str, email: str, password: str) -> User:
+    hashed = hash_password(password)
+    new:user = User(
+        username=username,
+        email=email,
+        hashed_password=hashed
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+def authenticate_user(db: Session, email: str, password: str) -> User | None:
+    user = get_user_by_email(db, email)
+    if not user:
+        return None
+    if not verify_password(password, user.hash_password):
+        return None
+    return user
+
+def generate_user_token(user: User) -> dict:
+    token = create_access_token({"sub": user.email})
+    return {"access_token": token, "token_type": "bearer"}
